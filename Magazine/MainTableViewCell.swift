a7239a6fafa4b83b4fdeb5e740cc660b02f8f8ec
YeoSungeun <dutjddms@naver.com>
2024-05-26 16:21:27 +0900

    Magazine MainTableView 구성, MainTabelViewCell 구성

--- Magazine/MainTableViewCell.swift
+++ Magazine/MainTableViewCell.swift
@@ -9,15 +9,12 @@ import UIKit
 
 class MainTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    @IBOutlet var photoImageView: UIImageView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
 }
--- Magazine/MainTableViewController.swift
+++ Magazine/MainTableViewController.swift
@@ -6,84 +6,68 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MainTableViewController: UITableViewController {
+    
+    var magazineList = MagazineInfo().magazine
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        self.title = "SeSAC TRAVEL"
+        
+        // 2. 셀 높이
+        tableView.rowHeight = 500
+  
     }
+    
+    func dateFormat(dateString: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyMMdd"
+        let date = dateFormatter.date(from: dateString) // dateString Date로 변환
 
-    // MARK: - Table view data source
+        dateFormatter.dateFormat = "yy월 MM월 dd일"
+        let mydate = dateFormatter.string(from: date!) // Date String으로 변환
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return mydate
+
     }
-
+    
+    // 1.셀 개수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        print(#function)
+        
+        return magazineList.count
     }
-
-    /*
+    
+    // 3. 셀 디자인 / 셀 데이터 넣기
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        print(#function, indexPath)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as! MainTableViewCell
+        
+        let url = URL(string: magazineList[indexPath.row].photo_image)
+        cell.photoImageView.kf.setImage(with: url)
+        cell.photoImageView.contentMode = .scaleAspectFill
+        cell.photoImageView.layer.cornerRadius = 10
+        
+        cell.titleLabel.text = magazineList[indexPath.row].title
+        cell.titleLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        cell.titleLabel.numberOfLines = 0
+        
+        cell.subTitleLabel.text = magazineList[indexPath.row].subtitle
+        cell.subTitleLabel.textColor = .gray
+        cell.subTitleLabel.font = .systemFont(ofSize: 14, weight: .semibold)
+        
+        cell.dateLabel.text = dateFormat(dateString: magazineList[indexPath.row].date)
+        cell.dateLabel.textColor = .gray
+        cell.dateLabel.font = .systemFont(ofSize: 12)
+        
+        
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
